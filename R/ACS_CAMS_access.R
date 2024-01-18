49254fdfba215c1dfc49db42fedde4de0553c29c
Johannes Heisig <johannes.heisig@gmail.com>
2024-01-18 15:41:13 +0100

    automate CAMS/ERA5 retrieval with ecmwfr

--- R/ACS_CAMS_access.R
+++ R/ACS_CAMS_access.R
@@ -1,12 +1,13 @@
 library(ecmwfr)
-source("R/functions.R")
+#source("R/functions.R")
 source("R/ecmwf_login.R")
 
+options(keyring_backend="file")
 wf_set_key(user = uid_cams, key = key_cams, service = "ads")
 
-stations = arrow::read_parquet("AQ_stations/EEA_stations_meta.parquet")
+#stations = arrow::read_parquet("AQ_stations/EEA_stations_meta.parquet")
 
-box = c(72, -25, 30, 35) # north, west, south, east
+box = c(72, -25, 30, 45) # north, west, south, east
 variables = c('nitrogen_dioxide', 'ozone', 'particulate_matter_10um',
               'particulate_matter_2.5um')
 
@@ -27,18 +28,24 @@ variables = c('nitrogen_dioxide', 'ozone', 'particulate_matter_10um',
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
-parse_request = function(year, var, dataset = "reanalysis", bbox = box,
-                         month = paste0(c(paste0(0,1:9), 10))){
+parse_request = function(year, var, semester, bbox = box){
+  monthdate = switch(semester,
+                     "1" = list(month = c(paste0(0,1:6)), 
+                              date = paste0(year,"-01-01/",year,"-06-30")),
+                     "2" = list(month = paste0(c(paste0(0,7:9), 10:12)), 
+                              date = paste0(year,"-07-01/",year,"-12-31")))
+  
+  dataset = ifelse(year < 2023, "reanalysis", "forecast")
   req = switch(dataset,
                "reanalysis" = list(
-                 year = year, month = month, variable = var, model = 'ensemble',
+                 year = year, month = monthdate[["month"]], variable = var, model = 'ensemble',
                  level = 0, format = "zip", type = ifelse(year < 2021, 'validated_reanalysis', 'interim_reanalysis'), 
                  dataset_short_name = 'cams-europe-air-quality-reanalyses', 
-                 target = paste0("supplementary/cams/EU_", var, "_hrly_", year,"_reanalysis.nc")),
+                 target = paste0("CAMS_", var, "_hrly_", year,"_reanalysis_", semester,".nc")),
                "forecast" = list(
-                   date = paste0(year,"-01-01/",year,"-12-31"), time = paste0(c(paste0(0,0:9), 10:23), ":00"),
+                   date = monthdate[["date"]], time = paste0(c(paste0(0,0:9), 10:23), ":00"),
                    variable = var, leadtime_hour = 0, level = 0, area = box, 
-                   target = paste0("supplementary/cams/EU_", var, "_hrly_", year,"_forecast.nc"),
+                   target = paste0("CAMS_", var, "_hrly_", year,"_forecast_", semester,".nc"),
                    format = "netcdf", type = 'analysis', model = 'ensemble',
                    dataset_short_name = 'cams-europe-air-quality-forecasts')
                 )
@@ -46,17 +53,45 @@ parse_request = function(year, var, dataset = "reanalysis", bbox = box,
 }
 
 
-# send multiple requests to ECMWF to keep requests and downloads small.
-for (y in 2022:2015){
-  for(v in variables){
-    request <- parse_request(y,v, "reanalysis")#, month = 10:12)
-    
-    file <- wf_request(
-      user     = uid_cams,   # user ID (for authentification)
-      request  = request,  # the request
-      transfer = F,     # download the file
-      path     = "."       # store data in current working directory
-    )
+# create and store requests
+for (y in 2023:2015){
+  for (v in variables){
+    for (s in c("1", "2")){
+      request = parse_request(y, v, s, box)#, month = 10:12)
+      
+      cams_req = wf_request(
+        user     = uid_cams,   # user ID (for authentification)
+        request  = request,  # the request
+        transfer = F,     # download the file
+        path     = "supplementary/cams_download"       # store data in current working directory
+      )
+      saveRDS(cams_req, paste0("supplementary/cams_requests/request_",v, "_", y,"_", s, ".rds"))
+      message(request$target)
+    }
   }
+}
+
+# download in case request has been processed ("completed")
+reqs = list.files("supplementary/cams_requests", full.names = T)
+for (r in reqs) {
+  req = readRDS(r)
+  req = req$update_status()
+  file = file.path("supplementary/cams_download", req$get_request()$target)
+  beepr::beep_on_error({
+    if (req$get_status() == "completed") {
+      message(basename(file))
+      if (!file.exists(file)){
+        job::job({
+          source("R/ecmwf_login.R")
+          #req$.__enclos_env__$private$file = basename(file)
+          req$.__enclos_env__$private$path = "supplementary/cams_download"
+          req$download(verbose = T)
+        }, import = "auto", packages = "ecmwfr", title = basename(file))
+        message(file, ": now downloading.")
+      }
+    } else {
+      message(file, ": downloaded.")
+    }
+  })
 }
 
--- R/CCS_ERA5_access.R
+++ R/CCS_ERA5_access.R
@@ -1,21 +1,25 @@
 library(ecmwfr)
-source("functions.R")
-source("R/ecmwf_login.R")
+#source("functions.R")
+options(keyring_backend="file")
 
-wf_set_key(user = uid_era, key = key_era, service = "cds")
+#source("R/ecmwf_login.R")
+# wf_set_key(user = uid_era, key = key_era, service = "cds")
 
-stations = arrow::read_parquet("AQ_stations/EEA_stations_meta.parquet")
-box = make_ecwmf_box(c(range(stations$Longitude), 
-                       range(stations$Latitude)), buffer = 2, view = T)
+box = c(72, -25, 30, 45)
 
-variables = c('10m_u_component_of_wind', '10m_v_component_of_wind',
-              '2m_dewpoint_temperature', '2m_temperature',
+variables = c('10m_u_component_of_wind', 
+              '10m_v_component_of_wind',
+              '2m_dewpoint_temperature', 
+              '2m_temperature',
               'surface_net_solar_radiation')
 
+
+# create and store requests
 for (y in 2022:2015){
   for(v in variables){
-    file = paste0("supplementary/era5/EU_", v, "clim_hrly_", y,".nc")
-    request <- list(
+    file = paste0("supplementary/era5_download/ERA5_", v, "_hrly_", y,".nc")
+    
+    request = list(
       date = paste0(y,"-01-01/",y,"-12-31"),
       format = "netcdf",
       variable = v,
@@ -24,17 +28,59 @@ for (y in 2022:2015){
       dataset_short_name = "reanalysis-era5-land",
       target = file
     )
-    
-    # If you have stored your user login information
-    # in the keyring by calling cds_set_key you can
-    # call:
-    file <- wf_request(
-      user     = "149688",   # user ID (for authentification)
-      request  = request,  # the request
-      transfer = F,     # download the file
-      path     = "."       # store data in current working directory
-    )
+    if (!file.exists(file)){
+      
+        dl = wf_request(
+          request  = request,  
+          user     = "149688",   # user ID (for authentification)
+          transfer = F,          # download the file
+          time_out = 3600*3,
+          path     = file
+        )
+        print(dl)
+        saveRDS(dl, paste0("supplementary/era5_requests/request_",v, "_", y,".rds"))
+    }
   }
 }
+
+# download in case request has been processed ("completed")
+reqs = list.files("supplementary/era5_requests", full.names = T)
+for (r in reqs) {
+  req = readRDS(r)
+  req = req$update_status()
+  if (req$get_status() == "completed") {
+    file = req$get_request()$target
+    message(file)
+    job::job({
+      source("R/ecmwf_login.R")
+      req$.__enclos_env__$private$file = file
+      req$.__enclos_env__$private$path = req$.__enclos_env__$private$path |> dirname()
+      req$download(verbose = T)
+    }, import = "auto", packages = "ecmwfr", title = basename(file))
+  }
+}
+
+
+
+
+
+
+# single file download jobs
+# y = 2023
+# v = 'surface_net_solar_radiation'
+# url = "https://download-0015-clone.copernicus-climate.eu/cache-compute-0015/cache/data8/adaptor.mars.internal-1705573865.506481-10021-7-94229913-8418-4ab8-9fad-306efec40ebf.nc"
+# file = paste0("supplementary/era5_download/ERA5_", v, "_hrly_", y,".nc")
+# job::job({
+#   options(timeout = 36000)
+#   download.file(url, file)
+#   }, 
+#   title = basename(file), import=NULL)
+
+
+
+
+
+
+
 
 
