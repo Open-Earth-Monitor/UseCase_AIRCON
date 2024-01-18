d95a5c645ae7bfa37e8f6fe9b3b5af2141538809
Johannes Heisig <johannes.heisig@gmail.com>
2024-01-18 08:23:06 +0100

    AQ data update 2023

--- R/EEA_AQ_data_access_all_countries.R
+++ R/EEA_AQ_data_access_all_countries.R
@@ -11,21 +11,14 @@ pollutants = c(7,8,5,6001)
 
 dl_file = "download_urls_all_countries.txt" 
 
-# generate_download_urls(countries, pollutants, 2015, 2023, file = "dl_file)
-# check_station_urls(dl_file)
+#generate_download_urls(countries, pollutants, year_start = 2015, year_end = 2023, file = dl_file)
+#check_station_urls(dl_file)
 files = download_station_data(dl_file) # 2 errors in 87342 files
 
 
 # read, filter, join pollutants for 2015-2023
 measurement_counts = mclapply(countries, preprocess_AQ_by_country, 
-                              dl_files = files, outdir = "AQ_data/01_hourly_gaps", 
+                              dl_files = files, 
+                              outdir = "AQ_data/01_hourly_gaps", 
                               mc.cores = 4)
-
-
-
-
-
-
-
-
 
